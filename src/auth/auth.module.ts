50cd7707dc10b9185da0c31d7ae8e14db855269b
reza <rzxsism80@gmail.com>
2023-04-15 20:08:40 +0330

    cqrs module implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { commandHandlers } from "./commands/handler";
+import { queryHandlers } from "./query/handler";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, ...commandHandlers, ...queryHandlers],
 })
 export class AuthModule {}
