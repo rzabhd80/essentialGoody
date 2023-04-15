a3bb1a612cd6b4241661f5fdaa404aecf9353ba6
reza <rzxsism80@gmail.com>
2023-04-15 19:45:02 +0330

    project reconfigured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.API_PORT || 4000;
+  const globalPrefix = "api";
+  app.setGlobalPrefix(globalPrefix);
+
   await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle("Code Challenge")
+    .setDescription("The cats API description")
+    .setVersion("1.0")
+    .addTag("cats")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 }
+
 bootstrap();
