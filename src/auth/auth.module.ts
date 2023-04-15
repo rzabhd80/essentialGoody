9a5c79c009c832f98f4f0aeddbc32c2219e8015e
reza <rzxsism80@gmail.com>
2023-04-15 20:13:18 +0330

    cqrs modules for all services implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,10 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { commandHandlers } from "./commands/handler";
 import { queryHandlers } from "./query/handler";
+import { CqrsModule } from "@nestjs/cqrs";
 
 @Module({
+  imports: [CqrsModule],
   controllers: [AuthController],
   providers: [AuthService, ...commandHandlers, ...queryHandlers],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
+import { CqrsModule } from "@nestjs/cqrs";
+import { commandHandlers } from "../auth/commands/handler";
+import { queryHandlers } from "./query/handler";
 
 @Module({
-  providers: [UsersService],
+  imports: [CqrsModule],
+  providers: [UsersService, ...commandHandlers, ...queryHandlers],
   controllers: [UsersController],
 })
 export class UsersModule {}
